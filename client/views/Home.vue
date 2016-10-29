7ac78c0303c18574ba4725ae860b62f19fe6b82b
ZengYuhang <312384485@qq.com>
2016-10-29 16:49:02 +0800

    改用.vue

---
+++ client/views/Home.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+  <div class="page">
+    <Counter />
+    <p>
+      To get started, edit files in <code>./client</code> and save.
+    </p>
+  </div>
+</template>
+
+<script>
+import Counter from '../components/Counter'
+export default {
+  components:{
+    Counter
+  }
+}
+</script>
+
+<style>
+
+</style>
